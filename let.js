154202dc3aaa0a040c49ed3bafbfbb92a2835b16
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-02-29 21:45:40 +0500

    Add files via upload

---
+++ let.js
@@ -0,0 +1,5 @@
+"use strict";
+// let variable declare that can be reassigned to different value. 
+let message = "Hello Taha ";
+message = "nice to meet you";
+console.log(message);
---
+++ let.ts
@@ -0,0 +1,4 @@
+// let variable declare that can be reassigned to different value. 
+let message = "Hello Taha ";
+  message = "nice to meet you"
+console.log(message);  
