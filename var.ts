ba24aa3463749be185647f397dec6ecc90ce2ed8
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-01 13:28:13 +0500

    Add files via upload

---
+++ var.ts
@@ -0,0 +1,5 @@
+// In Typescript var keyword used for variable declaration, with var declaration it doesn't matter ,
+//  how many times a variable declared and re-declarations are allowed.
+var message1 ="hello";
+var message1 ="how are you";
+console.log(message1);
