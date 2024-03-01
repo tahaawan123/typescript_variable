cc474bf15f61358255459bf3e70fee63a0e351f8
MUHAMMAD TAHA <160233035+tahaawan123@users.noreply.github.com>
2024-03-01 12:55:57 +0500

    Add files via upload

---
+++ const.ts
@@ -0,0 +1,4 @@
+// In Typescript  const is use to declare variables as constants ,meaning  when using const the variable
+//  must be initialized at the time of declaration,and its value can not be changed later on.
+const firstName ="Taha";
+console.log(firstName);
